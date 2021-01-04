1ff36167703bd8cc94db11c7bb2763b8eb3e9797
Danilo Araujo Sutier <67532755+danilosutier@users.noreply.github.com>
2021-01-04 17:01:39 -0300

    mudei localhost para 3000

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home(){
-    return <div>Home Page Danilo Sutier</div>
+function Home() {
+  return (
+    <div>
+      
+      <h1>Danilo Araujo Sutier  2020</h1>
+      <p>HOME PAGE AQUI!</p>
+      
+    </div>
+  )
 }
 
 export default Home
