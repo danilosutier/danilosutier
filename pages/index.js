cfeff0f22061a5f1a0a848dfae7b1393c450ba1d
Danilo Araujo Sutier <67532755+danilosutier@users.noreply.github.com>
2021-01-04 21:22:30 -0300

    index.html teste

--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,6 @@ function Home() {
         <div>
         <h1>Danilo Araujo Sutier  2020</h1>
         <p>HOME PAGE AQUI!</p>
-
-     <div class="main">
-      <div class="circle red"></div>
-      <div class="circle yellow"></div>
-      <div class="circle blue"></div>
-      <div class="circle green"></div>
-      <div class="circle red"></div>
-      <div class="circle yellow"></div>
-      <div class="circle blue"></div>
-      <div class="circle green"></div>
-    </div>
-    <div class="utilities">
-      <div class="title">
-        <h1 id="title">Christmas Lights</h1>
-      </div>
-      <div class="buttons">
-        <button id="play">On</button>
-        <button id="stop">Off</button>
-        <label for="">Speed:</label>
-         <input type="number" id="quantity" name="quantity" min="1" max="5" placeholder="From 1 until 5 only." value="">
-         <input type="submit" id="submit" name="" value="RUN" onclick="convert()">
-      </div>
-    </div>
         </div>
     )
 }
