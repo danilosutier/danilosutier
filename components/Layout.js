82198591c1b799c770c801d4c5b613e2adea76b9
Danilo Araujo Sutier <67532755+danilosutier@users.noreply.github.com>
2021-01-09 21:45:41 -0300

    att09012020

--- components/Layout.js
+++ components/Layout.js
@@ -10,12 +10,12 @@ import "./index.scss";
 import navButtons from "../config/buttons";
 
 const Layout = props => {
-  const appTitle = `> WHATABYTE`;
+  const appTitle = `> Danilo Sutier`;
 
   return (
     <div className="Layout">
       <Head>
-        <title>WHATABYTE</title>
+        <title>DANDANDAN</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
       </Head>
